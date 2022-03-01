4b5bd3ba4366627d9d184d6bffee65e1a2c69754
turtle <97395319+healtcliff@users.noreply.github.com>
2022-03-01 20:02:36 +0800

    Update helloworld.java

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -2,7 +2,7 @@ package helloworld;
 
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("hellow World!!!!!");
+		System.out.println("hellow World!!!!! this is my training to become a better coder !!!");
 	}
 
 }
